3c4c026b3455efba568164fec88b83a1b1d8af70
Zach Waite <zach@waiteperspectives.com>
2022-11-05 08:10:57 -0400

    change output to /docs to try using with github pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = function (env, argv) {
     mode: mode,
     entry: "./src/main.js",
     output: {
-      path: path.resolve(__dirname, "dist"),
+      path: path.resolve(__dirname, "docs"),
       filename: "main.js",
     },
     module: {
