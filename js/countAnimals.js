efb8cf1550d4f30a89323f68647c105c0a4f2001
Zach Waite <zach@waiteperspectives.com>
2023-10-10 17:28:53 -0400

    work on count the animal

--- js/countAnimals.js
+++ js/countAnimals.js
@@ -18,7 +18,8 @@ const removeDigit = (current) => {
         return "";
     } else {
         return current.slice(0, -1);
-    };
+    }
+    ;
 };
 
 const getAnimalsCounted = () => {
@@ -29,24 +30,6 @@ const setAnimalsCounted = (val) => {
     document.getElementById("animals-counted").value = val;
 };
 
-const handleNumpadEvent = numpadEvent => {
-    let newVal = "";
-    switch (numpadEvent.type) {
-        case "Add":
-            newVal = addDigit(getAnimalsCounted(), numpadEvent.data.value);
-            setAnimalsCounted(newVal);
-            break;
-        case "Remove":
-            newVal = removeDigit(getAnimalsCounted());
-            setAnimalsCounted(newVal);
-            break;
-        case "Submit":
-            console.log('submit');
-            break;
-        default:
-            console.error(`Unhandled numpad event ${numpadEvent}`);
-    }
-};
 
 const numberBtnClickHandler = e => {
     const evt = new NumpadEvent("Add", {value: e.target.dataset.number});
@@ -63,11 +46,134 @@ const submitBtnClickHandler = e => {
     handleNumpadEvent(evt);
 };
 
-export const preLoadHook = () => {};
+const clearBtnClickHandler = e => {
+    const evt = new NumpadEvent("Clear", {});
+    handleNumpadEvent(evt);
+};
+
+const randomNumberOfAnimals = () => {
+    return Math.ceil(Math.random() * 10);
+};
+
+const displayAnimalIcons = (numberOfAnimals) => {
+    const el = document.getElementById("animals-to-count");
+    el.innerHTML = "";
+    for (let i = 0; i < numberOfAnimals; i++) {
+        const span = document.createElement("span");
+        span.classList.add("p-3");
+        span.classList.add("fa");
+        span.classList.add("animate__animated")
+        span.classList.add("fa-hippo");
+        el.appendChild(span);
+        span.classList.add("animate__slideInDown");
+    }
+    for (let i = 0; i < numberOfAnimals; i++) {
+        setTimeout(() => {
+            el.children[i].classList.remove("animate__slideInDown");
+        }, 500);
+    }
+};
+
+const setCorrectAnimalCount = numberOfAnimals => {
+    const el = document.getElementById("animals-to-count");
+    el.dataset.animals = numberOfAnimals;
+};
+
+const getCorrectAnimalCount = () => {
+    const el = document.getElementById("animals-to-count");
+    return el.dataset.animals;
+};
+
+const celebrate = (rightAnswer) => {
+    const el = document.getElementById("animals-to-count");
+    let delay = 0;
+    for (let i = 0; i < el.children.length; i++) {
+        const span = el.children[i];
+        setTimeout(() => {
+                span.classList.add("animate__bounce");
+            }, delay
+        );
+        delay += 50;
+    }
+    delay += 100;
+    for (let i = 0; i < el.children.length; i++) {
+        const span = el.children[i];
+        setTimeout(() => {
+                span.classList.add("animate__bounceOutRight");
+            }, delay
+        );
+        delay += 50;
+    }
+    setTimeout(() => {
+        const el = document.getElementById("animals-to-count");
+        el.innerHTML = `<h4>Congratulations! ${rightAnswer} was the correct answer!</h4>`;
+    }, delay + 1000);
+    delay += 2000;
+    setTimeout(newGame, delay);
+};
+
+const educate = (rightAnswer) => {
+    const el = document.getElementById("animals-to-count");
+    let delay = 0;
+    for (let i = 0; i < el.children.length; i++) {
+        const span = el.children[i];
+        setTimeout(() => {
+                span.classList.add("animate__rotateOut");
+            }, delay
+        );
+        delay += 50;
+    }
+    setTimeout(() => {
+        const el = document.getElementById("animals-to-count");
+        el.innerHTML = `<h4>Sorry. The answer was ${rightAnswer}</h4>`;
+    }, delay + 1000);
+    delay += 2000;
+    setTimeout(newGame, delay);
+};
+
+
+const handleNumpadEvent = numpadEvent => {
+    let newVal = "";
+    switch (numpadEvent.type) {
+        case "Add":
+            newVal = addDigit(getAnimalsCounted(), numpadEvent.data.value);
+            setAnimalsCounted(newVal);
+            break;
+        case "Remove":
+            newVal = removeDigit(getAnimalsCounted());
+            setAnimalsCounted(newVal);
+            break;
+        case "Clear":
+            setAnimalsCounted("");
+            break;
+        case "Submit":
+            const correctAnswer = getCorrectAnimalCount();
+            const guess = getAnimalsCounted();
+            if (guess === correctAnswer) {
+                celebrate(correctAnswer);
+            } else {
+                educate(correctAnswer);
+            }
+            break;
+        default:
+            console.error(`Unhandled numpad event ${numpadEvent}`);
+    }
+};
+
+const newGame = () => {
+    const numberOfAnimals = randomNumberOfAnimals();
+    displayAnimalIcons(numberOfAnimals);
+    setCorrectAnimalCount(numberOfAnimals);
+    clearBtnClickHandler({});
+};
+
+export const preLoadHook = () => {
+};
 export const postLoadHook = () => {
     document.querySelectorAll(".number-btn").forEach(btn => {
         btn.addEventListener("click", numberBtnClickHandler);
     });
     document.querySelector(".remove-btn").addEventListener("click", removeBtnClickHandler);
     document.querySelector(".submit-btn").addEventListener("click", submitBtnClickHandler);
+    newGame();
 };
